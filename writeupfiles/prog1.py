52c0a41e9c24f23c0f6b69ff9f68e6cbeb9961f2
K@b00M! <43230227+cybernatichaxor@users.noreply.github.com>
2020-01-05 13:31:09 +1100

    Update prog1.py

--- writeupfiles/prog1.py
+++ writeupfiles/prog1.py
@@ -3,11 +3,6 @@ import math
 
 
 def CountTriangles(n): 
-  
-    #n = len(A); 
-  
-    #A.sort();  
-  
     count = 0; 
       
     for i in range(n, 1, -1): 
@@ -15,21 +10,11 @@ def CountTriangles(n):
         r = i - 1; 
         while(l < r): 
             if(l + r > i): 
-  
-                # If it is possible with a[l], a[r] 
-                # and a[i] then it is also possible 
-                # with a[l+1]..a[r-1],a[r] and a[i] 
                 count += r - l;  
-  
-                # checking for more possible solutions 
                 r -= 1;  
               
             else: 
-  
-                # if not possible check for  
-                # higher values of arr[l] 
                 l += 1;  
-    #print("No of possible solutions: ", count);
     return count;
 
 def CountTriangles2(n):
@@ -46,10 +31,7 @@ while(count < 4):
     print conn.recvuntil('n = ')
     n = conn.recvline()
     print n
-    #print conn.recvline()
     conn.sendline(str(CountTriangles2(int(n))))
     count = count + 1
-    #print conn.recvline()
 
 print conn.recvall()
-#print CountTriangles2(9999)
